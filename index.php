e6decd811a7a20f7fe533eb4459bdfaed7053a4d
lautaro <baumannlautaro9@gmail.com>
2025-11-21 16:24:33 +0100

    Añadido formulario de contacto experimental

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 echo "Hola Universo";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <form action="adas" method="POST">
+    <label> Nombre</label>
+    <input type="text" name="nombre" >
+</form>     
+
+</body>
+</html>
